1165ad6e9fd30e8901bb431adab31f130e325ac2
Gouiseong <rhdmltjd1324@gmail.com>
2025-11-08 01:25:54 +0900

    feat: 해야 할 일 입력 시 todo 리스트에 추가되는 기능 구현 중

--- src/main/java/com/example/demo/ToDoListApplication.java
+++ src/main/java/com/example/demo/ToDoListApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToDoListApplication {
-
+	// ToDo리스트
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
+		// SpringApplication.run(ToDoListApplication.class, args);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("오늘 할 일을 적어주세요");
+		String ToDo = sc.nextLine();
+		
+		String[] ToDo_list = new String[10];
+		for(int i=0;i<10;i++) {
+			ToDo_list[i]=ToDo;
+		}
+		System.out.print("오늘 할 일 : ");
+		System.out.println(ToDo);
+		
 	}
 
 }
